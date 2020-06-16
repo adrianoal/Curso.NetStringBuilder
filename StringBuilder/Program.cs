3b19b89acc905339fd245812d6028f2f58fc7161
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-16 11:51:05 -0300

    Resolucao de problema com a classe StringBuilder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(DateTime.Parse("15/06/2020 11:13:44"),
+                                              "Traveling to new Zealand",
+                                              "I'm going to visit this wonderful country",12
+                                              );
+            // Adicionando os dois comentario no Post:
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+
+            Comment c3 = new Comment("Good noght");
+            Comment c4 = new Comment("May the force be with you");
+            Post p2 = new Post(DateTime.Parse("16/06/2020 11:28:02"),
+                               "Good night guys",
+                               "See you tomorow",5
+                               );
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            // Imprimir o conteudo na tela de cada post com os comentarios:
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+
+
+        }
+    }
+}
